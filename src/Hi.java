ceba79bb3b116c4d4d559ca467f622a200583e1f
Roman <rpaniotov@yandex.ru>
2015-02-23 23:11:32 +0300

    1

--- src/Hi.java
+++ src/Hi.java
@@ -6,6 +6,11 @@ public class Hi
     public static void main(String[] args)
     {
         for(int i = 0; i < 5; i++)
-        System.out.println("Привет мир");
+        {
+            System.out.println(5);
+            System.out.println("Привет мир");
+
+            // тестовый коммит номер два
+        }
     }
 }
